28a4ab4ba8143de74a56d44e60c8cebe124fd63a
vdella <vitor.origamer@gmail.com>
2022-09-22 22:54:16 -0300

    connected enums with otp read and write

--- include/machine/riscv/sifive_u/sifive_u_otp.h
+++ include/machine/riscv/sifive_u/sifive_u_otp.h
@@ -68,13 +68,9 @@ public:
 public:
     OTP() {}
 
-    static int sifive_otp_read(struct udevice *dev, int offset,
-                               void *buf, int size)
+    static int sifive_otp_read(int offset, void *buf, int size)
     {
-        struct sifive_otp_plat *plat = dev_get_plat(dev);
-        struct sifive_otp_regs *regs = (struct sifive_otp_regs *) plat->regs;
-
-        /* Check if offset and size are multiple of BYTES_PER_FUSE */
+        // Check if offset and size are multiple of BYTES_PER_FUSE.
         if ((size % BYTES_PER_FUSE) || (offset % BYTES_PER_FUSE)) {
             printf("%s: size and offset must be multiple of 4.\n", __func__);
             return -EINVAL;
@@ -83,7 +79,7 @@ public:
         int fuse_id = offset / BYTES_PER_FUSE;
         int fuse_count = size / BYTES_PER_FUSE;
 
-        /* check bounds */
+        // Check bounds.
         if (offset < 0 || size < 0)
             return -EINVAL;
         if (fuse_id >= plat->total_fuses)
@@ -93,49 +89,45 @@ public:
 
         int fuse_buf[fuse_count];
 
-        /* init OTP */
-        writel(PDSTB_DEEP_STANDBY_ENABLE, &regs->pdstb);
-        writel(PTRIM_ENABLE_INPUT, &regs->ptrim);
-        writel(PCE_ENABLE_INPUT, &regs->pce);
+        // Init OTP.
+        writel(PDSTB_DEEP_STANDBY_ENABLE, OTP::PDSTB);
+        writel(PTRIM_ENABLE_INPUT, OTP::PTRIM);
+        writel(PCE_ENABLE_INPUT, OTP::PCE);
 
-        /* read all requested fuses */
+        // Read all requested fuses.
         for (unsigned int i = 0; i < fuse_count; i++, fuse_id++) {
-            writel(fuse_id, &regs->pa);
+            writel(fuse_id, OTP::PA);
 
-            /* cycle clock to read */
-            writel(PCLK_ENABLE_VAL, &regs->pclk);
+            // Cycle clock to read.
+            writel(PCLK_ENABLE_VAL, OTP::PCLK);
             ndelay(TCD_DELAY * 1000);
-            writel(PCLK_DISABLE_VAL, &regs->pclk);
+            writel(PCLK_DISABLE_VAL, OTP::PCLK);
             ndelay(TKL_DELAY * 1000);
 
-            /* read the value */
-            fuse_buf[i] = readl(&regs->pdout);
+            // Read the value.
+            fuse_buf[i] = readl(OTP::PDOUT);
         }
 
-        /* shut down */
-        writel(PCE_DISABLE_INPUT, &regs->pce);
-        writel(PTRIM_DISABLE_INPUT, &regs->ptrim);
-        writel(PDSTB_DEEP_STANDBY_DISABLE, &regs->pdstb);
+        // Shut down.
+        writel(PCE_DISABLE_INPUT, OTP::PCE);
+        writel(PTRIM_DISABLE_INPUT, OTP::PTRIM);
+        writel(PDSTB_DEEP_STANDBY_DISABLE, OTP::PDSTB);
 
-        /* copy out */
+        // Copy out.
         memcpy(buf, fuse_buf, size);
 
         return size;
     }
 
     /*
- * Caution:
- * OTP can be written only once, so use carefully.
- *
- * offset and size are assumed aligned to the size of the fuses (32-bit).
- */
-    static int sifive_otp_write(struct udevice *dev, int offset,
-                                const void *buf, int size)
+    * Caution:
+    * OTP can be written only once, so use carefully.
+    *
+    * offset and size are assumed aligned to the size of the fuses (32-bit).
+    */
+    static int sifive_otp_write(int offset, const void *buf, int size)
     {
-        struct sifive_otp_plat *plat = dev_get_plat(dev);
-        struct sifive_otp_regs *regs = (struct sifive_otp_regs *)plat->regs;
-
-        /* Check if offset and size are multiple of BYTES_PER_FUSE */
+        // Check if offset and size are multiple of BYTES_PER_FUSE.
         if ((size % BYTES_PER_FUSE) || (offset % BYTES_PER_FUSE)) {
             printf("%s: size and offset must be multiple of 4.\n",
                    __func__);
@@ -148,7 +140,7 @@ public:
         Reg32 write_data;
         int i, pas, bit;
 
-        /* check bounds */
+        // Check bounds.
         if (offset < 0 || size < 0)
             return -EINVAL;
         if (fuse_id >= plat->total_fuses)
@@ -156,55 +148,54 @@ public:
         if ((fuse_id + fuse_count) > plat->total_fuses)
             return -EINVAL;
 
-        /* init OTP */
-        writel(PDSTB_DEEP_STANDBY_ENABLE, &regs->pdstb);
-        writel(PTRIM_ENABLE_INPUT, &regs->ptrim);
+        // Init OTP.
+        writel(PDSTB_DEEP_STANDBY_ENABLE, OTP::PDSTB);
+        writel(PTRIM_ENABLE_INPUT, OTP::PTRIM);
 
-        /* reset registers */
-        writel(PCLK_DISABLE_VAL, &regs->pclk);
-        writel(PA_RESET_VAL, &regs->pa);
-        writel(PAS_RESET_VAL, &regs->pas);
-        writel(PAIO_RESET_VAL, &regs->paio);
-        writel(PDIN_RESET_VAL, &regs->pdin);
-        writel(PWE_WRITE_DISABLE, &regs->pwe);
-        writel(PTM_FUSE_PROGRAM_VAL, &regs->ptm);
+        // Reset registers.
+        writel(PCLK_DISABLE_VAL, OTP::PCLK);
+        writel(PA_RESET_VAL, OTP::PA);
+        writel(PAS_RESET_VAL, OTP::PAS);
+        writel(PAIO_RESET_VAL, OTP::PAIO);
+        writel(PDIN_RESET_VAL, OTP::PDIN);
+        writel(PWE_WRITE_DISABLE, OTP::PWE);
+        writel(PTM_FUSE_PROGRAM_VAL, OTP::PTM);
         ndelay(TMS_DELAY * 1000);
 
-        writel(PCE_ENABLE_INPUT, &regs->pce);
-        writel(PPROG_ENABLE_INPUT, &regs->pprog);
+        writel(PCE_ENABLE_INPUT, OTP::PCE);
+        writel(PPROG_ENABLE_INPUT, OTP::PPROG);
 
-        /* write all requested fuses */
+        // Write all requested fuses.
         for (i = 0; i < fuse_count; i++, fuse_id++) {
-            writel(fuse_id, &regs->pa);
-            write_data = *(write_buf++);
+            writel(fuse_id, OTP::PA);
+            Reg32 write_data = *(write_buf++);
 
             for (pas = 0; pas < 2; pas++) {
-                writel(pas, &regs->pas);
+                writel(pas, OTP::PAS);
 
                 for (bit = 0; bit < 32; bit++) {
-                    writel(bit, &regs->paio);
-                    writel(((write_data >> bit) & 1),
-                           &regs->pdin);
+                    writel(bit, OTP::PAIO);
+                    writel(((write_data >> bit) & 1), OTP::PDIN);
                     ndelay(TASP_DELAY * 1000);
 
-                    writel(PWE_WRITE_ENABLE, &regs->pwe);
+                    writel(PWE_WRITE_ENABLE, OTP::PWE);
                     udelay(TPW_DELAY);
-                    writel(PWE_WRITE_DISABLE, &regs->pwe);
+                    writel(PWE_WRITE_DISABLE, OTP::PWE);
                     udelay(TPWI_DELAY);
                 }
             }
 
-            writel(PAS_RESET_VAL, &regs->pas);
+            writel(PAS_RESET_VAL, OTP::PAS);
         }
 
-        /* shut down */
-        writel(PWE_WRITE_DISABLE, &regs->pwe);
-        writel(PPROG_DISABLE_INPUT, &regs->pprog);
-        writel(PCE_DISABLE_INPUT, &regs->pce);
-        writel(PTM_RESET_VAL, &regs->ptm);
+        // Shut down.
+        writel(PWE_WRITE_DISABLE, OTP::PWE);
+        writel(PPROG_DISABLE_INPUT, OTP::PPROG);
+        writel(PCE_DISABLE_INPUT, OTP::PCE);
+        writel(PTM_RESET_VAL, OTP::PTM);
 
-        writel(PTRIM_DISABLE_INPUT, &regs->ptrim);
-        writel(PDSTB_DEEP_STANDBY_DISABLE, &regs->pdstb);
+        writel(PTRIM_DISABLE_INPUT, OTP::PTRIM);
+        writel(PDSTB_DEEP_STANDBY_DISABLE, OTP::PDSTB);
 
         return size;
     }
