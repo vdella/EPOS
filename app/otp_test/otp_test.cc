e8c1b71bf73679312ad7e3214945e3d6c263bc87
vdella <vitor.origamer@gmail.com>
2022-09-25 22:01:56 -0300

    added otp test template

---
+++ app/otp_test/otp_test.cc
@@ -0,0 +1,13 @@
+// EPOS RISC-V 64 Test Program
+
+#include <machine/otp.h>
+
+using namespace EPOS;
+
+int main()
+{
+    OStream cout;
+    cout << "RISC-V 64-bits SiFiveFU540 OTP test" << endl;
+    OTP otp;
+    return 0;
+}
