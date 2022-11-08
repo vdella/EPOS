7d982f1270b3a1fe8c15c8fdd62d6c669d642182
vdella <vitor.origamer@gmail.com>
2022-11-08 18:35:50 -0300

    added multitask flag

--- app/segment_test/segment_test_traits.h
+++ app/segment_test/segment_test_traits.h
@@ -14,12 +14,12 @@ template<> struct Traits<Build>: public Traits_Tokens
     static const unsigned int MACHINE = RISCV;
     static const unsigned int MODEL = SiFive_U;
     static const unsigned int CPUS = 1;
-    static const unsigned int NODES = 1; // (> 1 => NETWORKING)
-    static const unsigned int EXPECTED_SIMULATION_TIME = 60; // s (0 => not simulated)
+    static const unsigned int NODES = 1;  // (> 1 => NETWORKING)
+    static const unsigned int EXPECTED_SIMULATION_TIME = 100000000;  // s (0 => not simulated)
 
     // Default flags
     static const bool enabled = true;
-    static const bool monitored = false;
+    static const bool monitored = true;
     static const bool debugged = true;
     static const bool hysterically_debugged = false;
 
@@ -104,7 +104,8 @@ template<> struct Traits<System>: public Traits<Build>
 {
     static const unsigned int mode = Traits<Build>::MODE;
     static const bool multithread = (Traits<Application>::MAX_THREADS > 1);
-    static const bool multiheap = true;
+    static const bool multiheap = Traits<Scratchpad>::enabled;
+    static const bool multitask = !Traits<Scratchpad>::enabled;
 
     static const unsigned long LIFE_SPAN = 1 * YEAR; // s
     static const unsigned int DUTY_CYCLE = 1000000; // ppm
@@ -120,9 +121,9 @@ template<> struct Traits<Thread>: public Traits<Build>
     static const bool enabled = Traits<System>::multithread;
     static const bool trace_idle = hysterically_debugged;
     static const bool simulate_capacity = false;
-    static const unsigned int QUANTUM = 100000; // us
+    static const unsigned int QUANTUM = 10000; // us
 
-    typedef RR Criterion;
+    typedef FCFS Criterion;
 };
 
 template<> struct Traits<Scheduler<Thread>>: public Traits<Build>
