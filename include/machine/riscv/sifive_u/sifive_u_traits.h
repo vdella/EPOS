0a19bf300244e6c9a9c85eb7cdac704aaee7fc0d
vdella <vitor.origamer@gmail.com>
2022-11-21 21:00:50 -0300

    added IMAGE addr when not in LIBRARY

--- include/machine/riscv/sifive_u/sifive_u_traits.h
+++ include/machine/riscv/sifive_u/sifive_u_traits.h
@@ -34,7 +34,7 @@ public:
     // Physical Memory at Boot
     static const unsigned long BOOT = NOT_USED;
     static const unsigned long SETUP = library ? NOT_USED : RAM_BASE; // RAM_BASE (will be part of the free memory at INIT, using a logical address identical to physical eliminate SETUP relocation)
-    static const unsigned long IMAGE = NOT_USED;            // RAM_BASE + 1 MB (will be part of the free memory at INIT, defines the maximum image size; if larger than 3 MB then adjust at SETUP)
+    static const unsigned long IMAGE = library ? NOT_USED : 0x0000000080300000;            // RAM_BASE + 1 MB (will be part of the free memory at INIT, defines the maximum image size; if larger than 3 MB then adjust at SETUP)
 
     // Logical Memory
     static const unsigned long APP_LOW = library ? RAM_BASE : RAM_TOP+1; // 2 GB + 4 MB         0x00000000ff7ffff
@@ -47,8 +47,8 @@ public:
     static const unsigned long INIT = library ? NOT_USED : 0x0000000080200000; // RAM_BASE + 512 KB (will be part of the free memory at INIT)
     static const unsigned long PHY_MEM = NOT_USED;                   // 512 MB (max 1536 MB of RAM)
     static const unsigned long IO       = NOT_USED;                        // 0 (max 512 MB of IO = MIO_TOP - MIO_BASE)
-    static const unsigned long SYS      = 0x0000000080300000;
-    static const unsigned long SYS_DATA = 0x0000000080400000;
+    static const unsigned long SYS      = 0x0000000080400000;
+    static const unsigned long SYS_DATA = 0x0000000080600000;
 
     // Default Sizes and Quantities
     static const unsigned int MAX_THREADS = 16;
